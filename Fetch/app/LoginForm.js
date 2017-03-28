0cf97ede56696d92c4e5fe3c08e9a0beaa501874
Md Anil Shankar Jethani <ajethani11@gmail.com>
2017-03-27 21:42:53 -0700

    Finished with login screen

---
+++ Fetch/app/LoginForm.js
@@ -0,0 +1,65 @@
+/**
+ * Created by AnilJethani on 3/27/17.
+ */
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    KeyboardAvoidingView
+} from 'react-native';
+
+
+export default class LoginForm extends Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <TextInput style={styles.input}
+            placeholder={'username or email'}
+            placeholderTextColor='rgba(255,255,255,0.7)'
+            autoCapitalize='none'
+            autoCorrect={false}
+            keyboardType= 'email-address'
+            returnKeyType='next'
+          />
+          <TextInput style={styles.input}
+            placeholder={'password'}
+            placeholderTextColor='rgba(255,255,255,0.7)'
+            secureTextEntry
+            returnKeyType='go'
+          />
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>
+                LOGIN
+            </Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      padding: 20
+    },
+    input: {
+      height: 40,
+      backgroundColor: 'rgba(255,255,255,0.2)',
+      marginBottom: 10,
+      color: '#FFF',
+      paddingHorizontal: 10
+    },
+    buttonContainer: {
+      backgroundColor: '#2980b9',
+      paddingVertical: 15,
+    },
+    buttonText: {
+      color: '#FFF',
+      textAlign: 'center',
+      fontWeight: '700'
+    },
+});
--- Fetch/app/index.js
+++ Fetch/app/index.js
@@ -3,30 +3,57 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  KeyboardAvoidingView
 } from 'react-native';
+import LoginForm from './LoginForm';
 
 
 export default class Fetch extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Fetch!
-        </Text>
-      </View>
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <View style={styles.container}>
+          <View style={styles.logoContainer}>
+            <Image style={styles.logo}
+              source={require('../images/gl-5-triton.png')}
+            />
+            <Text style={styles.textImage}>
+              An app made for UCSD using React Native
+            </Text>
+          </View>
+          <View>
+            <LoginForm />
+          </View>
+        </View>
+      </KeyboardAvoidingView>
     );
   }
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+    container: {
+      flex: 1,
+      backgroundColor: '#3498db'
+    },
+    logo: {
+      width: 100,
+      height: 100,
+    },
+    logoContainer: {
+      flexGrow: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textImage: {
+      color: '#FFF',
+      marginTop: 10,
+      width: 160,
+      textAlign: 'center',
+      opacity: 0.8
+    }
 });
 
 AppRegistry.registerComponent('Fetch', () => Fetch);
